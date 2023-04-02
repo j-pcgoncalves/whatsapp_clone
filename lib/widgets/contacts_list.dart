c39f0a9e8869d314407e0777e195778b8f086d2c
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-04-02 19:02:50 +0100

    style contacts list

--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
 import 'package:whatsapp_clone/info.dart';
 
 class ContactsList extends StatelessWidget {
@@ -9,20 +10,47 @@ class ContactsList extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
       child: ListView.builder(
+        shrinkWrap: true,
         itemCount: info.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              info[index]['name'].toString(),
-              style: const TextStyle(fontSize: 18),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 6),
-              child: Text(
-                info[index]['message'].toString(),
-                style: const TextStyle(fontSize: 15),
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: ListTile(
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 6),
+                      child: Text(
+                        info[index]['message'].toString(),
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                    ),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        info[index]['profilePic'].toString(),
+                      ),
+                    ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
+              const Divider(
+                color: dividerColor,
+                indent: 85,
+              ),
+            ],
           );
         },
       ),
