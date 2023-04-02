03e8beb73c209e873249a95f7255ce80ebd9bae5
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-04-02 18:33:20 +0100

    create web screen and mobile screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/colors.dart';
 import 'package:whatsapp_clone/responsive/responsive_layout.dart';
+import 'package:whatsapp_clone/screens/mobile_screen_layout.dart';
+import 'package:whatsapp_clone/screens/web_screen_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,9 +20,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         backgroundColor: backgroundColor,
       ),
-      home: ResponsiveLayout(
-          mobileScreenLayout: mobileScreenLayout,
-          webScreenLayout: webScreenLayout),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
     );
   }
 }
